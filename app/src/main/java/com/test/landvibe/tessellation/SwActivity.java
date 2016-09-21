9f1063150bd0895822534ea1ed15dc8d470d9039
koseungbin <kosb1563@gmail.com>
2016-09-22 03:35:31 +0900

    Animation 구조 수정

--- app/src/main/java/com/test/landvibe/tessellation/SwActivity.java
+++ app/src/main/java/com/test/landvibe/tessellation/SwActivity.java
@@ -69,177 +69,81 @@ public class SwActivity extends Activity {
 
                 if ((action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_MOVE) && gv.getChildAt(position) != null) {
 
+
                     Animation animation = new AlphaAnimation(0.0f,1.0f);
                     animation.setDuration(1000);
                     gv.getChildAt(position).setAnimation(animation);
 
+                    int draw_temp = 0;
+
                     if(SESSION.equals("SESSION1")) {
-                        gv.getChildAt(position).setBackgroundResource(position % 2 == 0 ? R.drawable.scene_1_1_1 : R.drawable.scene_1_1_2);
+                        draw_temp = (position % 2 == 0 ? R.drawable.scene_1_1_1 : R.drawable.scene_1_1_2);
                     }
                     else if(SESSION.equals("SESSION2")) {
 
-                        switch(position % 24){
-                            case 0 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_01);
-                                break;
-                            case 1 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_02);
-                                break;
-                            case 2 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_03);
-                                break;
-                            case 3 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_04);
-                                break;
-                            case 4 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_05);
-                                break;
-                            case 5 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_06);
-                                break;
-                            case 6 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_07);
-                                break;
-                            case 7 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_08);
-                                break;
-                            case 8 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_09);
-                                break;
-                            case 9 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_10);
-                                break;
-                            case 10 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_11);
-                                break;
-                            case 11 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_12);
-                                break;
-                            case 12 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_13);
-                                break;
-                            case 13 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_14);
-                                break;
-                            case 14 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_15);
-                                break;
-                            case 15 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_16);
-                                break;
-                            case 16 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_17);
-                                break;
-                            case 17 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_18);
-                                break;
-                            case 18 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_19);
-                                break;
-                            case 19 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_20);
-                                break;
-                            case 20 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_21);
-                                break;
-                            case 21 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_22);
-                                break;
-                            case 22 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_23);
-                                break;
-                            case 23 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_2_24);
-                                break;
-                            default:
-                                break;
-                        }
+                       if(position % 24 < 12) {
+                           draw_temp = (position % 2 == 0 ? R.drawable.scene_1_2_03 : R.drawable.scene_1_2_02);
+                       }else {
+                           draw_temp = (position % 2 == 0 ? R.drawable.scene_1_2_13 : R.drawable.scene_1_2_14);
+                       }
+
 
                     }else if (SESSION.equals("SESSION3")){
 
 
-                        switch(position % 24){
-                            case 0 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_01);
-                                break;
-                            case 1 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_02);
-                                break;
-                            case 2 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_03);
-                                break;
-                            case 3 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_04);
-                                break;
-                            case 4 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_05);
-                                break;
-                            case 5 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_06);
-                                break;
-                            case 6 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_07);
-                                break;
-                            case 7 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_08);
-                                break;
-                            case 8 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_09);
-                                break;
-                            case 9 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_10);
-                                break;
-                            case 10 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_11);
-                                break;
-                            case 11 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_12);
-                                break;
-                            case 12 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_13);
-                                break;
-                            case 13 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_14);
-                                break;
-                            case 14 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_15);
-                                break;
-                            case 15 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_16);
-                                break;
-                            case 16 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_17);
-                                break;
-                            case 17 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_18);
-                                break;
-                            case 18 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_19);
-                                break;
-                            case 19 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_20);
-                                break;
-                            case 20 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_21);
-                                break;
-                            case 21 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_22);
-                                break;
-                            case 22 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_23);
-                                break;
-                            case 23 :
-                                gv.getChildAt(position).setBackgroundResource(R.drawable.scene_1_3_24);
-                                break;
-                            default:
-                                break;
+                        if(position % 24 < 12) {
+                            switch(position % 6){
+                                case 0 :
+                                    draw_temp = R.drawable.scene_1_3_07;
+                                    break;
+                                case 1 :
+                                    draw_temp = R.drawable.scene_1_3_02;
+                                    break;
+                                case 2 :
+                                    draw_temp = R.drawable.scene_1_3_03;
+                                    break;
+                                case 3 :
+                                    draw_temp = R.drawable.scene_1_3_04;
+                                    break;
+                                case 4 :
+                                    draw_temp = R.drawable.scene_1_3_05;
+                                    break;
+                                case 5 :
+                                    draw_temp = R.drawable.scene_1_3_06;
+                                    break;
+                                default:
+                                    ;
+                            }
+
+                        }else {
+                            switch(position % 5) {
+                                case 0:
+                                    draw_temp = R.drawable.scene_1_3_13;
+                                    break;
+                                case 1:
+                                    draw_temp = R.drawable.scene_1_3_14;
+                                    break;
+                                case 2:
+                                    draw_temp = R.drawable.scene_1_3_15;
+                                    break;
+                                case 3:
+                                    draw_temp = R.drawable.scene_1_3_16;
+                                    break;
+                                case 4:
+                                    draw_temp = R.drawable.scene_1_3_17;
+                                    break;
+                                default:
+                                    ;
+                            }
                         }
 
                     }else{
-
+                        ;
                     }
 
+                    gv.getChildAt(position).setBackgroundResource(draw_temp);
                 }
+
                 return true;
             }
         });
