1466aaafd03e7ad93db84553f6712c8e648c6944
positiwee <positiwee@gmail.com>
2016-10-30 13:32:25 +0900

    Merge branch 'master' of https://github.com/KOSEUNGBIN/Tessellation
    Conflicts:
    	app/build.gradle
    	aa

--- app/src/main/java/com/test/landvibe/tessellation/MainActivity.java
+++ app/src/main/java/com/test/landvibe/tessellation/MainActivity.java
@@ -1,136 +1,54 @@
 package com.test.landvibe.tessellation;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.AbsoluteLayout;
-import android.widget.ImageView;
-
-import static android.view.MotionEvent.ACTION_CANCEL;
-import static android.view.MotionEvent.ACTION_DOWN;
-import static android.view.MotionEvent.ACTION_HOVER_ENTER;
-import static android.view.MotionEvent.ACTION_HOVER_EXIT;
-import static android.view.MotionEvent.ACTION_HOVER_MOVE;
-import static android.view.MotionEvent.ACTION_MOVE;
-import static android.view.MotionEvent.ACTION_OUTSIDE;
-import static android.view.MotionEvent.ACTION_UP;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
-
-    ImageView img1;
-    ImageView img2;
-    ImageView img3;
-    ImageView img4;
-    ImageView img5;
-
-    int img_width = 100;
-    int img_height = 100;
-
-    //사용할 패턴 이미지
-    Bitmap patternImg;
-    Bitmap resize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        //bitmap으로 이미지 가져와서 사이즈 조절
-        patternImg = BitmapFactory.decodeResource(getApplication().getResources(), R.drawable.pattern_image);
-        resize = Bitmap.createScaledBitmap(patternImg, img_width, img_height, true);
+        final ImageButton button = (ImageButton) findViewById(R.id.entrance);
 
-        //레이아웃 만들기
-//        LinearLayout linearLayout = new LinearLayout(this);
-//        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams
-//                (ViewGroup.LayoutParams.MATCH_PARENT,
-//                        ViewGroup.LayoutParams.MATCH_PARENT);
-//        linearLayout.setLayoutParams(params);
+        buttonEffect(button);
 
-        AbsoluteLayout absoluteLayout = new AbsoluteLayout(this);
-
-        img1 = new ImageView(this);
-        img2 = new ImageView(this);
-        img3 = new ImageView(this);
-        img4 = new ImageView(this);
-        img5 = new ImageView(this);
-
-        absoluteLayout.addView(img1, new AbsoluteLayout.LayoutParams(img_width, img_height, 0, 0));
-        absoluteLayout.addView(img2, new AbsoluteLayout.LayoutParams(img_width, img_height, 100, 100));
-        absoluteLayout.addView(img3, new AbsoluteLayout.LayoutParams(img_width, img_height, 200, 200));
-        absoluteLayout.addView(img4, new AbsoluteLayout.LayoutParams(img_width, img_height, 300, 300));
-        absoluteLayout.addView(img5, new AbsoluteLayout.LayoutParams(img_width, img_height, 400, 400));
-
-//        //만들어 놓은 LinearLayout에 imageview들 추가
-//        linearLayout.addView(img1);
-//        linearLayout.addView(img2);
-//        linearLayout.addView(img3);
-//        linearLayout.addView(img4);
-//        linearLayout.addView(img5);
-
-        setContentView(absoluteLayout);
-
-        //TouchListener 추가
-        TouchListener touchListener1 = new TouchListener("linearLayout");
-        absoluteLayout.setOnTouchListener(touchListener1);
-    }
-
-    class TouchListener implements View.OnTouchListener {
-        private String imgv_name;//어떤 이미지뷰에서 발생하는지 알기위한 변수
-
-        TouchListener(String imgv_name) {
-            this.imgv_name = imgv_name;
-        }
-
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            switch (motionEvent.getActionMasked()) {
-                case ACTION_DOWN:
-                    Log.e("MainActivity", "ACTION_DOWN 발생");
-                    break;
-                case ACTION_HOVER_ENTER:
-                    Log.e("MainActivity", "ACTION_HOVER_ENTER 발생");
-                    break;
-                case ACTION_HOVER_MOVE:
-                    Log.e("MainActivity", "ACTION_HOVER_MOVE 발생");
-                    break;
-                case ACTION_HOVER_EXIT:
-                    Log.e("MainActivity", "ACTION_HOVER_EXIT 발생");
-                    break;
-                case ACTION_MOVE:
-                    Log.e("MainActivity", "ACTION_MOVE 발생");
-                    float x = motionEvent.getX();
-                    float y = motionEvent.getY();
-
-                    Log.v(imgv_name, "X = " + x + "     Y = " + y);
-
-                    if (x < 100 && y < 100) {
-                        img1.setImageBitmap(resize);
-                    } else if (100 < x && x < 200 && 100 < y && y < 200) {
-                        img2.setImageBitmap(resize);
-                    } else if (200 < x && x < 300 && 200 < y && y < 300) {
-                        img3.setImageBitmap(resize);
-                    } else if (300 < x && x < 400 && 300 < y && y < 400) {
-                        img4.setImageBitmap(resize);
-                    } else if (400 < x && x < 500 && 400 < y && y < 500) {
-                        img5.setImageBitmap(resize);
-                    }
-
-                    break;
-                case ACTION_CANCEL:
-                    Log.e("MainActivity", "ACTION_CANCEL 발생");
-                    break;
-                case ACTION_UP:
-                    Log.e("MainActivity", "ACTION_UP 발생");
-                    break;
-                case ACTION_OUTSIDE:
-                    Log.d("MainActivity", "ACTION_OUTSIDE 발생");
-                    break;
-
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(intent);
             }
-            return true;
-        }
+        });
+
     }
+
+    public static void buttonEffect(View button){
+        button.setOnTouchListener(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        v.getBackground().setColorFilter(0x60000000, PorterDuff.Mode.SRC_ATOP);
+                        v.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP: {
+                        v.getBackground().clearColorFilter();
+                        v.invalidate();
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
+    }
+
+
 }
---
+++ app/src/main/java/com/test/landvibe/tessellation/MenuActivity.java
@@ -0,0 +1,240 @@
+package com.test.landvibe.tessellation;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class MenuActivity extends Activity {
+    private float xCurrentPos, yCurrentPos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        final ImageButton button = (ImageButton) findViewById(R.id.map_korea);
+        final ImageButton button2 = (ImageButton) findViewById(R.id.map_book);
+        final ImageView cloud1 = (ImageView) findViewById(R.id.cloud01);
+        final ImageView cloud2 = (ImageView) findViewById(R.id.cloud02);
+        final ImageView cloud3 = (ImageView) findViewById(R.id.cloud03);
+        final ImageView cloud4 = (ImageView) findViewById(R.id.cloud04);
+        final ImageView cloud5 = (ImageView) findViewById(R.id.cloud05);
+        final ImageView cloud6 = (ImageView) findViewById(R.id.cloud06);
+
+        buttonEffect(button);
+        buttonEffect(button2);
+
+        final Animation anim= new TranslateAnimation(xCurrentPos, xCurrentPos+150, yCurrentPos, yCurrentPos);
+        anim.setDuration(5000);
+        anim.setFillAfter(true);
+        anim.setRepeatCount(100);
+        anim.setRepeatMode(Animation.REVERSE);
+        anim.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation arg0) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation arg0) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+
+            }
+        });
+        final Animation anim2= new TranslateAnimation(xCurrentPos, xCurrentPos-150, yCurrentPos, yCurrentPos);
+        anim2.setDuration(4000);
+        anim2.setFillAfter(true);
+        anim2.setRepeatCount(100);
+        anim2.setRepeatMode(Animation.REVERSE);
+        anim2.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation arg0) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation arg0) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+
+            }
+        });
+
+        final Animation anim3= new TranslateAnimation(xCurrentPos, xCurrentPos-150, yCurrentPos, yCurrentPos);
+        anim3.setDuration(3000);
+        anim3.setFillAfter(true);
+        anim3.setRepeatCount(100);
+        anim3.setRepeatMode(Animation.REVERSE);
+        anim3.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation arg0) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation arg0) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+
+            }
+        });
+
+        final Animation anim4= new TranslateAnimation(xCurrentPos, xCurrentPos+150, yCurrentPos, yCurrentPos);
+        anim4.setDuration(2500);
+        anim4.setFillAfter(true);
+        anim4.setRepeatCount(100);
+        anim4.setRepeatMode(Animation.REVERSE);
+        anim4.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation arg0) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation arg0) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+
+            }
+        });
+
+        final Animation anim5= new TranslateAnimation(xCurrentPos, xCurrentPos+150, yCurrentPos, yCurrentPos);
+        anim5.setDuration(6000);
+        anim5.setFillAfter(true);
+        anim5.setRepeatCount(100);
+        anim5.setRepeatMode(Animation.REVERSE);
+        anim5.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation arg0) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation arg0) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+
+            }
+        });
+
+        final Animation anim6= new TranslateAnimation(xCurrentPos, xCurrentPos+150, yCurrentPos, yCurrentPos);
+        anim6.setDuration(4500);
+        anim6.setFillAfter(true);
+        anim6.setRepeatCount(100);
+        anim6.setRepeatMode(Animation.REVERSE);
+        anim6.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation arg0) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation arg0) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+
+            }
+        });
+
+        float xPos[] = new float[6];
+        float yPos[] = new float[6];
+
+        xPos[0] =  cloud1.getLeft();
+        yPos[0] = cloud1.getTop();
+        xPos[1] =  cloud2.getLeft();
+        yPos[1] = cloud2.getTop();
+        xPos[2] =  cloud3.getLeft();
+        yPos[2] = cloud3.getTop();
+        xPos[3] =  cloud4.getLeft();
+        yPos[3] = cloud4.getTop();
+        xPos[4] =  cloud5.getLeft();
+        yPos[4] = cloud5.getTop();
+        xPos[5] =  cloud6.getLeft();
+        yPos[5] = cloud6.getTop();
+
+        xPos[0] = cloud1.getLeft();
+        yPos[0] = cloud1.getTop();
+        cloud1.startAnimation(anim3);
+        xPos[1] = cloud2.getLeft();
+        yPos[1] = cloud2.getTop();
+        cloud2.startAnimation(anim);
+        xPos[2] = cloud3.getLeft();
+        yPos[2] = cloud3.getTop();
+        cloud3.startAnimation(anim6);
+        xPos[3] = cloud4.getLeft();
+        yPos[3] = cloud4.getTop();
+        cloud4.startAnimation(anim4);
+        xPos[4] = cloud5.getLeft();
+        yPos[4] = cloud5.getTop();
+        cloud5.startAnimation(anim5);
+        xPos[5] = cloud6.getLeft();
+        yPos[5] = cloud6.getTop();
+        cloud6.startAnimation(anim2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, SwActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, Sw3Activity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public static void buttonEffect(View button){
+        button.setOnTouchListener(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        v.getBackground().setColorFilter(0x60000000, PorterDuff.Mode.SRC_ATOP);
+                        v.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP: {
+                        v.getBackground().clearColorFilter();
+                        v.invalidate();
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
+    }
+
+
+}
